addb9174f42b7dff9f6fc356e07914e02fb69281
David Burns <david@lullabot.com>
2013-03-15 23:20:40 -0400

    attempting to add user to the database during registration

--- models.py
+++ models.py
@@ -9,6 +9,7 @@ class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(80), unique=True)
   email = db.Column(db.String(120), unique=True)
+  password = db.Column(db.String(120), unique=True)
   moods = db.relationship('Mood', backref='user', lazy='dynamic')
   events = db.relationship('Event', backref='user', lazy='dynamic')
   tags = db.relationship('Tag', backref='user', lazy='dynamic')
--- moody-app.py
+++ moody-app.py
@@ -7,11 +7,16 @@ Flask-Login, including remember me functionality.
 
 :copyright: (C) 2011 by Matthew Frazier.
 :license:   MIT/X11, see LICENSE for more details.
+
+Modified from flask-user-system
+https://github.com/Liuchang0812/flask-user-system
 """
-from flask import Flask, request, render_template, redirect, url_for, flash
+from flask import Flask
+from flask import request, render_template, redirect, url_for, flash, session, escape
+from flask.ext import sqlalchemy
 from flask.ext.login import (LoginManager, current_user, login_required,
-                            login_user, logout_user, UserMixin, AnonymousUser,
-                            confirm_login, fresh_login_required)
+                            login_user, logout_user, session, UserMixin,
+                            AnonymousUser, confirm_login, fresh_login_required)
 
 class User(UserMixin):
     def __init__(self, name, id, active=True):
@@ -50,9 +55,20 @@ login_manager.login_view = "login"
 login_manager.login_message = u"Please log in to access this page."
 login_manager.refresh_view = "reauth"
 
-@login_manager.user_loader
-def load_user(id):
-    return USERS.get(int(id))
+# check the data
+def validate(newuser):
+    if len(newone.Password) < 6:
+        return 0
+    m = re.match('\w+@\w+\.\w+', str(newuser.Email))
+    if m is None:
+        print 'email is wrong'
+        return 0;
+
+    return 1
+
+# @login_manager.user_loader
+# def load_user(id):
+#     return USERS.get(int(id))
 
 
 login_manager.setup_app(app)
@@ -66,6 +82,41 @@ def index():
 @fresh_login_required
 def secret():
     return render_template("secret.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if 'username' in session:
+        flash("You shoult login out first!")
+        return redirect(url_for('info'))
+    if request.method == 'GET':
+        #print "get"
+        return render_template('register.html')
+    else:
+        #print "post"
+        username = request.form['username']
+        password = request.form['password']
+        repassword = request.form['repassword']
+        email = request.form['email']
+
+        #print "< %s %s %s>" % (username , password ,email)
+        if password != repassword:
+            flash("passwords doesn't match!!")
+            return render_template('register.html')
+
+        newuser = User(username, email, password)
+        #print newuser
+        if validate(newuser):
+            db.session.add(newuser)
+            db.session.commit()
+            flash("Registration is successful!")
+            session['username'] = newuser.Username
+            session['email'] = newuser.Email
+            return render_template('index.html',user=newuser)
+        else:
+            flash("Registration is not successful!")
+            return render_template('register.html')
+    return render_template("register.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
